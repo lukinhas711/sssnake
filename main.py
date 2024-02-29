3490986676fc83bf546b16d9f45164354dfea93b
Lucas <lucas.ribeiro711@gmail.com>
2024-02-29 10:58:44 -0300

    some vars

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pygame
 import random
 
 pygame.init()
-pygame.display.set_caption('Cobrinha')
-width, height = 1200, 900
+pygame.display.set_caption('Ssssnake 🐍')
+width, height = 1080, 720
 display = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
@@ -15,29 +15,25 @@ color_green = (0, 255, 0)
 square_size = 20
 game_speed = 15
 
-# loop
 def run_game():
     end_game = False
 
+    x = width / 2
+    y = height / 2
+
+    x_speed = 0
+    y_speed = 0
+
+    snake_size = 1
+    snake_foods = []
+
     while not end_game:
+        display.fill(color_black)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 end_game = True
 
-
-
-# interface
-# score
-# snake
-# food
-
-# logic to end game
-# self collision
-# wall collision
-
-# user interactions
-# close
-# keyboard
 
 
 run_game()
